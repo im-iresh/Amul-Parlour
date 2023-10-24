0dedc76559d1d1254058b01b5ff9872be5d87380
im-iresh <43372784+im-iresh@users.noreply.github.com>
2023-10-24 15:47:19 +0530

    1st commit

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery_app/screens/cart/cart_widget.dart';
+import 'package:grocery_app/widgets/text_widget.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/cart_provider.dart';
+import '../../providers/products_provider.dart';
+import '../../services/global_methods.dart';
+import '../../services/utils.dart';
+import '../../widgets/empty_screen.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Utils(context).color;
+    Size size = Utils(context).getScreenSize;
+    final cartProvider = Provider.of<CartProvider>(context);
+    final cartItemsList =
+    cartProvider.getCartItems.values.toList().reversed.toList();
+    return cartItemsList.isEmpty
+        ? const EmptyScreen(
+      title: 'Your cart is empty',
+      subtitle: 'Add something and make me happy :)',
+      buttonText: 'Shop now',
+      imagePath: 'assets/images/cart.png',
+    )
+        : Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          title: TextWidget(
+            text: 'Cart (${cartItemsList.length})',
+            color: color,
+            isTitle: true,
+            textSize: 22,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                GlobalMethods.warningDialog(
+                    title: 'Empty your cart?',
+                    subtitle: 'Are you sure?',
+                    fct: () {
+                      cartProvider.clearCart();
+                    },
+                    context: context);
+              },
+              icon: Icon(
+                IconlyBroken.delete,
+                color: color,
+              ),
+            ),
+          ]),
+      body: Column(
+        children: [
+          _checkout(ctx: context),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cartItemsList.length,
+              itemBuilder: (ctx, index) {
+                return ChangeNotifierProvider.value(
+                    value: cartItemsList[index],
+                    child: CartWidget(
+                      q: cartItemsList[index].quantity,
+                    ));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _checkout({required BuildContext ctx}) {
+    final Color color = Utils(ctx).color;
+    Size size = Utils(ctx).getScreenSize;
+    final cartProvider = Provider.of<CartProvider>(ctx);
+    final productProvider = Provider.of<ProductsProvider>(ctx);
+
+    double total = 0.0;
+    cartProvider.getCartItems.forEach((key, value) {
+      final getCurrProduct = productProvider.findProdById(value.productId);
+      total += (getCurrProduct.isOnSale
+          ? getCurrProduct.salePrice
+          : getCurrProduct.price) *
+          value.quantity;
+    });
+    return SizedBox(
+      width: double.infinity,
+      height: size.height * 0.1,
+      // color: ,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Row(children: [
+          Material(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextWidget(
+                  text: 'Order Now',
+                  textSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+          FittedBox(
+            child: TextWidget(
+              text: 'Total: \$${total.toStringAsFixed(2)}',
+              color: color,
+              textSize: 18,
+              isTitle: true,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
